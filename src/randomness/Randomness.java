bd7f0e76193cdb7ab6da0f6a3f7d75b76f6b6b44
Mike Schouten <justmike112@hotmail.com>
2016-11-23 13:49:30 +0100

    Added some more code

--- src/randomness/Randomness.java
+++ src/randomness/Randomness.java
@@ -17,29 +17,29 @@ public class Randomness {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        runAlot(10);
-        runAlot(100);
-        runAlot(200);
-        runAlot(1000);
-        runAlot(40000);
+        runAlot(10, 10);
+        runAlot(100,10);
+        runAlot(200,10);
+        runAlot(1000,10);
+        runAlot(40000,100);
         
     }
     
-    private static int randomThis() {
+    private static int randomThis(int max) {
         //add randomizer
         Random rand = new Random();
-        int randomReturn = rand.nextInt(10) + 1;
+        int randomReturn = rand.nextInt(max) + 1;
         return randomReturn;
     }
     
-    private static void runAlot(int times) {
+    private static void runAlot(int times, int randomTo) {
         int average = 0;
         for (int i=0; i<times; i++) {
-            average += randomThis();
+            average += randomThis(randomTo);
         }
         //System.out.println("the total number was " + average);
         average = average / times;
-        System.out.println("After " + times + " iterations, the average was " + average);
+        System.out.println("After " + times + " iterations, the average of 1 to "+ randomTo+" was " + average);
     }
     
     
